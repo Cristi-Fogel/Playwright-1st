5f5e2d33aab9e79d0223b9d4b5603ba962b8fad3
Cristi-Fogel <37100222+Cristi-Fogel@users.noreply.github.com>
2024-03-28 08:40:26 +0200

    e2e cart test
    - test failing if not in debug, possible issue @ for loop identifier

--- tests/RS_ClientApp.spec.js
+++ tests/RS_ClientApp.spec.js
@@ -19,13 +19,17 @@ test('Browser check', async ({page})=>
         if(await products.nth(i).locator("b").textContent() === searchForProductName)
         {
             //logic to add product to cart, since we've identified the one we search for
-            await products.nth(i).locator("text= Add To Cart").click();
+            await products.nth(i).locator("text= Add To Cart").click();  
             break; //found it, no need to continue loop
         }
     }
+    //goto cart
+    await page.waitForLoadState('networkidle')
+    await page.locator("button[routerlink='/dashboard/cart']").dispatchEvent('click');
+    await page.locator("div li").first().waitFor(); //wait until they show up (1st item in list is populated with data)
     
-    await page.locator("[routerlink*='cart'").click();
-    
+    const bool = await page.locator("h3:has-text('Zara Coat 3')").isVisible(); //check that it exists -- returns boolean value
 
-
+    expect(bool).toBeTruthy();  
 })
+ 
