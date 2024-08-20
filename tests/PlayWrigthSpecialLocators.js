27896f950727c2fc85cc3ee3cb8655d772d13b5d
Cristi-Fogel <37100222+Cristi-Fogel@users.noreply.github.com>
2024-08-20 09:11:30 +0300

    correct file extension, experiment with locator types

--- tests/PlayWrigthSpecialLocators.js
+++
@@ -1,8 +0,0 @@
-const {test, expect} = require('@playwright/test');
-
-test('Playwrigth SPecial Locators', async ({page})=>
-{
-    await page.goto("https://rahulshettyacademy.com/angularpractice/");
-    
-    
-}) 
---
+++ tests/PlaywrigthSpecialLocators.spec.js
@@ -0,0 +1,19 @@
+const {test, expect} = require('@playwright/test');
+
+test('Playwright locators check', async ({page})=>
+{
+    await page.goto("https://rahulshettyacademy.com/angularpractice/");
+    await page.getByLabel("Check me out if you Love IceCreams!").click(); //labelText
+    await page.getByLabel("Employed").check(); //checkbox will be check/uncheck
+    await page.getByLabel("Gender").selectOption("Female"); //select option from a dropdown
+    await page.getByPlaceholder("Password").fill("myPassword123");
+    await page.getByRole("button", {name: "submit"}).click();  //byRole, will filter all buttons, so give argument of what you want
+
+    await page.getByText("Success! The Form has been submitted successfully!.").isVisible();  //scans whole page 
+
+    await page.getByRole("link", {name: "Shop"}).click();
+    await page.locator("app-card").filter({hasText: 'Nokia Edge'}).getByRole("button").click(); //only one button in card, so no filtering needed here
+
+    
+    
+}) 
