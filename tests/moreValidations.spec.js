e1757506b4b2a2cf3bbea38197cce902c678417e
Cristi-Fogel <37100222+Cristi-Fogel@users.noreply.github.com>
2024-08-22 08:56:12 +0300

    iframe, hover, events
    start working on the api components/validations

--- tests/moreValidations.spec.js
+++ tests/moreValidations.spec.js
@@ -1,4 +1,5 @@
 const { test, expect } = require("@playwright/test");
+const { text } = require("stream/consumers");
 
 test("Popup validations", async({page})=>
 {
@@ -9,6 +10,18 @@ test("Popup validations", async({page})=>
     await expect(page.locator("#displayed-text")).toBeVisible();
     await page.locator("#hide-textbox").click();
     await expect(page.locator("#displayed-text")).toBeHidden();
+    
+    // java popup events(not web/html)
+    await page.locator("#confirmbtn").click();
+    page.on('dialog', dialog => dialog.accept());
+    // page.on('dialog', dialog => dialog.dismiss());
 
+    //hover elements
+    await page.locator("#mousehover").hover();
 
+    // change to iframes
+    const framesPage = page.frameLocator("#courses-iframe"); 
+    await framesPage.locator("li a[href*='lifetime-access']:visible").click(); //this will also scroll into view the element 
+    const textCheck = await framesPage.locator(".text h2").textContent(); //string is "join 13242 happy subscribers" -- so we split and grab what we need
+    console.log(textCheck.split(" ")[1]); //split on whitespace
 })
