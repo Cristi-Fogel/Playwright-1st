0d6ceedd9e562e1d51717907ed4431f2ad68bcd5
Cristi-Fogel <37100222+Cristi-Fogel@users.noreply.github.com>
2024-09-12 09:05:26 +0300

    cleanup code

--- tests/upload-download-spec.js
+++ tests/upload-download-spec.js
@@ -4,12 +4,9 @@ const {test, expect} = require('@playwright/test');
 async function writeExcelTest(searchText, replaceText, change, filePath){
     const workbook = new ExcelJs.Workbook();
     await workbook.xlsx.readFile(filePath);
-    
     const worksheet = workbook.getWorksheet('Sheet1'); // This needs to be declared before calling readExcel
-
-    // Call readExcel and wait for its completion
-    const output = await readExcel(worksheet, searchText, change);
-
+    const output = await readExcel(worksheet, searchText, change);     // Call readExcel and wait for its completion
+    //perform the change
     if (output.row !== -1 && output.column !== -1) {
         const cell = worksheet.getCell(output.row, output.column + change.colChange);
         cell.value = replaceText;
@@ -22,7 +19,6 @@ async function writeExcelTest(searchText, replaceText, change, filePath){
 
 async function readExcel(worksheet, searchText) {
     let output = { row: -1, column: -1 };
-
     worksheet.eachRow((row, rowNumber) => {
         row.eachCell((cell, colNumber) => {
             if (cell.value === searchText) {
@@ -31,13 +27,10 @@ async function readExcel(worksheet, searchText) {
             }
         });
     });
-
     return output;
 }
 
 //update mango price to 350
-
-
 // writeExcelTest("Republic", 350, {rowChange: 0, colChange:2}, "./files/excellDownloadTest.xlsx");
 
 
