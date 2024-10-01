ccb35e0c8832f92b055d7b5d91b0850d9e5bb746
Cristi-Fogel <37100222+Cristi-Fogel@users.noreply.github.com>
2024-10-01 08:50:46 +0300

    prepared config file for custom config/project tests

---
+++ playwright.config1.js
@@ -0,0 +1,53 @@
+// @ts-check
+const { defineConfig, devices } = require('@playwright/test');
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// require('dotenv').config();
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+module.exports = defineConfig({
+  testDir: './tests',
+  timeout: 30 * 1000, 
+  expect: {timeout: 5000},   
+  reporter: 'html',
+
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+
+
+  /* Configure projects for major browsers */
+  projects: [
+    { 
+      // to run:  
+      // D:\z_programming\auto\playwright\1stCourse> npx playwright test tests/RS_ClientAppPO.spec --config playwright.config1.js --project=safari    
+      name: 'safari',
+      use: {
+        browserName: 'webkit',
+        headless: false, 
+        screenshot: 'on',
+        trace: 'off',
+        ...devices['iPhone 11'], //explicit website
+        ignoreHTTPSErrors: true // SSL certification error auto-accept
+        
+      }
+    }, 
+    {  
+      name: 'chrome',
+      use: {
+        browserName: 'chromium',
+        headless: false, 
+        screenshot: 'off',
+        trace: 'off',
+        viewport: {width:720, height:720} //for explicit browser size #Responsive
+      }
+    }
+    
+  ],
+
+ 
+});
+
