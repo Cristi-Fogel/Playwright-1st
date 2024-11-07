a70c2da7dac00659eb0d3d6c66f1d77a30214275
Cristi-Fogel <37100222+Cristi-Fogel@users.noreply.github.com>
2024-11-07 09:03:27 +0200

    start practicing the TS part

---
+++ tests/demo.js
@@ -0,0 +1,9 @@
+let message = "Hello";
+message = 2;
+
+let age = 20;
+let isActive = true;
+let numbers = [1,2,3];
+
+let data = "string" + 1;
+data = 1;
---
+++ tests/demo1.ts
@@ -0,0 +1,9 @@
+let message1 : string = "hello";
+message1  = "just stings";
+
+let age1: number = 20;
+let isActive1: boolean = true;
+let numbers1: number[] = [1,2,3];
+
+let data1: any = "unkown data type";
+data1 = 42; // can accept diferent data type, as it :any
