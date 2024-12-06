48b5466c0b873872e33499869110e50fa38e901d
Cristi-Fogel <37100222+Cristi-Fogel@users.noreply.github.com>
2024-12-06 08:56:47 +0200

    add code to the hook so it can actually save a screenshot on error & create a 2nd feature file

--- features/support/hooks.js
+++ features/support/hooks.js
@@ -1,6 +1,6 @@
 const playwright =  require('playwright/test');
 const { POManager } = require("../../pageObjects/POManager");
-const { Before, After } = require ('@cucumber/cucumber');
+const { Before, After, BeforeStep, AfterStep, Status } = require ('@cucumber/cucumber');
 
 
 Before(async function() {
@@ -16,8 +16,10 @@ Before(async function() {
 BeforeStep(function(){
 
 });
-AfterStep(function({result}){
-    
+AfterStep(async function({result}){
+    if(result.status === Status.FAILED){
+        await this.page.screenshot({path: 'screenshot1.png'});
+    }
 });
 
 After (function(){
