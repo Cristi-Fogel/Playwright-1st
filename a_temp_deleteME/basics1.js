12de3715cece9a94f659089d363876f489455f0c
Cristi-Fogel <37100222+Cristi-Fogel@users.noreply.github.com>
2024-12-19 08:56:27 +0200

    basics - got to arrays

--- a_temp_deleteME/basics1.js
+++ a_temp_deleteME/basics1.js
@@ -48,8 +48,49 @@ while(required){
     required= false;
 }
 
-// from 1->10 give common multiple values of 2 and 5
+// from, and/or loops
 for(let k=0; k<=10; k++){
-    if(k%2 == 0)
-    console.log("for loop - multiple 2, 5: " + k)
+    if(k%2 == 0 && k%5 == 0)
+    console.log("for loop - multiple 2 AND 5: " + k)
 }
+for(let k=0; k<=10; k++){
+    if(k%2 == 0 || k%5 == 0)
+    console.log("for loop - multiple 2 OR 5: " + k)
+}
+
+let n = 0;
+for(let k=0; k<=100; k++){
+    if(k%2 == 0 && k%5 == 0){
+        n++;
+        console.log("for loop - multiple 2 OR 5 - first 3 characters: " + k);
+        if (n==3);
+        break;
+    }
+    
+}
+
+console.log("########################################################")
+console.log("ARRAYS");
+var marks = Array(6);
+var marks = new  Array(20, 40, 35, 12, 38);
+
+var marks = [20, 40, 35, 12, 38];
+mySubArray = marks.slice(2,5);
+console.log("subArray: " + mySubArray);
+
+console.log(marks[2]);
+
+marks[3] = 15;
+console.log(marks[3]);
+
+console.log("array length: " + marks.length);
+
+marks.push(65);//append
+console.log(marks);
+marks.pop();
+console.log(marks);
+marks.unshift(1); // 1st position
+
+console.log("number on posistion: " + marks.indexOf(38));
+console.log("120 part of array? " + marks.includes(120));
+
